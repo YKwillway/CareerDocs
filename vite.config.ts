3125bbff3e3dc1148a98e74e6ff997d53f7fed50
YKwillway <yuchan0708bicky@gmail.com>
2025-02-15 18:02:18 +0900

    ビルドファイルの命名規則を設定し、再ビルド

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // ビルドされたリソースのパスを正しく指定するためにスラッシュで囲む
-  base: "/CareerDocs/",
+  base: "/CareerDocs/", // リポジトリ名に合わせて設定
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name][ext][hash]",
+      },
+    },
+  },
 });
