140849a35c9355d18ec154c2e57a4cf9633a212a
YKwillway <yuchan0708bicky@gmail.com>
2025-02-15 18:39:20 +0900

    Vite設定ファイルとTypeScript設定ファイルを修正し、再ビルド

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/CareerDocs/", // リポジトリ名に合わせて設定
   plugins: [react()],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
   build: {
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/main.tsx"),
+      },
       output: {
         entryFileNames: "[name].js",
         chunkFileNames: "[name].js",
